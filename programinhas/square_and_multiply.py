87c911d0777a50fe0085275a36681b501e8c0a48
henrique <hqlc04@gmail.com>
2026-02-08 10:04:19 -0300

    2.c

--- programinhas/square_and_multiply.py
+++ programinhas/square_and_multiply.py
@@ -11,7 +11,7 @@ def square_and_multiply(a,exp,mod):
     base = a
     while exp != 0:
         if exp%2 == 1:
-            result*=base
+            result = (result*base)%mod
         exp//=2
         base = (base*base)%mod
     return result
@@ -23,11 +23,14 @@ MAX_K = 1000 # k máximo que estamos dispostos a tentar
 DIV = [] # divisores que achamos
 
 '''
-    Note que k*(2**(N+2)) + 1) é divisor  de 2{32} + 1 se, e só se, 2{32} = 1 mod  k*(2**(N+2)) + 1). Portanto podemos acha-los
+    Note que k*(2**(N+2)) + 1) é divisor  de 2{32} + 1 se, e só se, 2{32} + 1 = 0 mod  k*(2**(N+2)) + 1). Portanto podemos acha-los
     da seguinte forma.
 '''
+base = 2**(N+2)
 for k in range(1,MAX_K):
-    if (square_and_multiply(2,2**N, k*(2**(N+2)) + 1) - 1) == 0:
+    mod = k*base + 1
+    power2_mod = square_and_multiply(2,2**N, k*base+1) # =  2^{2^N} mod k*2^{N+2} + 1
+    if (power2_mod + 1)%(k*base + 1) == 0:
         DIV += [k*(2**(N+2)) + 1]
         print(f"{M} = 2^(2^{N}) + 1 = {DIV[-1]} * {M // DIV[-1]} : {DIV[-1]} = {k}*(2^{N+2}) + 1")
 
